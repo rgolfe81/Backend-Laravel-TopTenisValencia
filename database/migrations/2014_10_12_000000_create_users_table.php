77d342f258c1f823fb488c7783835c04367b8b28
Ruben <rgolfe81@gmail.com>
2023-04-13 13:09:28 +0200

    feat: roles migration and add FK to users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
