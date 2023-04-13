d5483eadae161e5c0e9c8bc4f2091906aef5a5cf
Ruben <rgolfe81@gmail.com>
2023-04-13 13:26:46 +0200

    feat: matches and results migrations with FKs

---
+++ database/migrations/2023_04_13_111829_create_results_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('match_id');
+            $table->foreign('match_id')
+                ->references('id')
+                ->on('matches')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('set_number');
+            $table->integer('score');
+            $table->boolean('winner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+};
