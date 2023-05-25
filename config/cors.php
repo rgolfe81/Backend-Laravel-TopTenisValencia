30f06feb3fd25a6cef49137e6a533355d676d1da
Ruben <rgolfe81@gmail.com>
2023-05-25 12:54:31 +0200

    fix: install fruitcake/laravel-cors

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET,POST,PUT,DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://frontend-react-top-tenis-valencia.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
